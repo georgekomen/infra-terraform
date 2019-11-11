77980ec77b302b8a20b9071913b3edb39833843f
komen <gkkomensi@gmail.com>
2019-11-11 08:29:07 +0300

    add provisioners for logrotate

--- terraform.tf
+++ terraform.tf
@@ -186,6 +186,7 @@ resource "aws_instance" "nginx1" {
   subnet_id = aws_subnet.subnet1.id
   key_name = var.key_name
   vpc_security_group_ids = [aws_security_group.nginx-instance-sg.id]
+  iam_instance_profile = aws_iam_instance_profile.nginx_profile.name
 
   connection {
       type = "ssh"
@@ -194,10 +195,48 @@ resource "aws_instance" "nginx1" {
       private_key = file(var.private_key_path)
   }
 
+  provisioner "file" {
+    content = <<EOF
+    access_key =
+    secret_key =
+    security_token =
+    use_https = True
+    bucket_location = US
+    EOF
+    destination = "/home/ec2-user/.s3cfg"
+  }
+
+  provisioner "file" {
+    content = <<EOF
+    /var/log/nginx/*log {
+      daily
+      rotate 10
+      missingok
+      compress
+      sharedscripts
+      postrotate
+      endscript
+      lastaction
+           INSTANCE_ID = `curl --silent http://169.254.169.254/latest/meta-data/instance-id`
+           sudo /usr/local/bin/s3cmd sync --config=/home/ec2-user/.s3cfg /var/log/nginx/ s3://${aws_s3_bucket.web_bucket.id}/nginx/$INSTANCE_ID/
+      endscript
+    }
+    EOF
+    destination = "/home/ec2-user/nginx"
+  }
+
   provisioner "remote-exec" {
       inline = [
           "sudo yum install nginx -y",
-          "sudo service nginx start"
+          "sudo service nginx start",
+          "sudo cp /home/ec2-user/.s3cfg /root/.s3cfg",
+          "sudo cp /home/ec2-user/nginx /etc/logrotate.d/nginx",
+          "sudo pip install s3cmd",
+          "s3cmd get s3://${aws_s3_bucket.web_bucket.id}/website/index.html .",
+          "s3cmd get s3://${aws_s3_bucket.web_bucket.id}/website/pic.png .",
+          "sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html",
+          "sudo cp /home/ec2-user/pic.png /usr/share/nginx/html/pic.png",
+          "sudo logrotate -f /etc/logrotate.conf"
       ]
   }
 }
